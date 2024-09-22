32bb8b73c555f2e9f4f21b536f8fb064c4ad7f8c
SECVBulRep <bfarukshin@gmail.com>
2024-09-22 15:36:08 +0300

    сделали проверки, посмотретли как оно работает в реальной реализщации . но увидели ч то  нашей оно не сработало

--- EnumDemo/Enum.ConsoleApp/Program.cs
+++ EnumDemo/Enum.ConsoleApp/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+//Enumerable.Select<int, int>(null, x => x * 2);
+Select<int, int>(null, x => x * 2);
+
+
 IEnumerable<int> source = Enumerable.Range(1, 10);
 
 foreach (var number in Select(source, x => x * 2))
@@ -7,9 +12,11 @@ foreach (var number in Select(source, x => x * 2))
     Console.WriteLine(number);
 }
 
-
 static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
 {
+    ArgumentNullException.ThrowIfNull(source);
+    ArgumentNullException.ThrowIfNull(selector);
+    
     foreach (var i in source)
     {
         yield return selector(i);
