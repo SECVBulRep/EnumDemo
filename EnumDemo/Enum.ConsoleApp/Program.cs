25297ad542a23fe85095080ac304ea1458061cc6
SECVBulRep <bfarukshin@gmail.com>
2024-09-22 15:44:33 +0300

    Тело не вызывает пока не вызван MoveNext/ показал пример того как вызывать эксепшн

--- EnumDemo/Enum.ConsoleApp/Program.cs
+++ EnumDemo/Enum.ConsoleApp/Program.cs
@@ -2,15 +2,14 @@
 
 
 //Enumerable.Select<int, int>(null, x => x * 2);
-Select<int, int>(null, x => x * 2);
 
 
-IEnumerable<int> source = Enumerable.Range(1, 10);
-
-foreach (var number in Select(source, x => x * 2))
-{
-    Console.WriteLine(number);
-}
+Console.WriteLine(0);
+IEnumerable<int> e = Select<int, int>(null, x => x * 2);
+Console.WriteLine(1);
+IEnumerator<int> enumerator = e.GetEnumerator();
+Console.WriteLine(2);
+enumerator.MoveNext();
 
 static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
 {
