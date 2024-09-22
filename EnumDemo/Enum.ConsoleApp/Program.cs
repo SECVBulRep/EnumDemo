a7d59fd567b6246496336c2c5f606093c7863455
SECVBulRep <bfarukshin@gmail.com>
2024-09-22 15:20:04 +0300

    Давайте реализуйем Select

--- EnumDemo/Enum.ConsoleApp/Program.cs
+++ EnumDemo/Enum.ConsoleApp/Program.cs
@@ -2,18 +2,7 @@
 
 Console.WriteLine("Hello, World!");
 
-IEnumerable<int> e = GetValues();
-using IEnumerator<int> enumerator = e.GetEnumerator();
-Console.WriteLine(enumerator);
-while (enumerator.MoveNext())
-{
-    int i = enumerator.Current;
-    Console.WriteLine(i);
-}
 
-static IEnumerable<int> GetValues()
+static IEnumerable<int> Select()
 {
-    yield return 1;
-    yield return 2;
-    yield return 3;
 }
