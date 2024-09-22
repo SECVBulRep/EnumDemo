7866ba856fdeaee528882cc3b6e5a2bbbd11df57
SECVBulRep <bfarukshin@gmail.com>
2024-09-22 14:57:12 +0300

    init

---
+++ EnumDemo/Enum.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+static IEnumerable<int> GetValues()
+{
+    return new List<int>() { 1, 2, 3 };
+}
